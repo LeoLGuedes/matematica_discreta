2a1c25a43916c3e6913db6e669b470b9e4031909
LeoLGuedes <leonardo.guedes.08@gmail.com>
2024-08-25 02:32:02 -0300

    enunciado e nome adicionado

--- calculador.py
+++ calculador.py
@@ -1,3 +1,18 @@
+#Para obter os pontos relativos a este trabalho, você deverá criar um programa, utilizando a
+#linguagem Python, C, ou C++. Este programa, quando executado, irá apresentar os resultados de
+#operações que serão realizadas entre dois conjuntos de dados.
+#O programa que você desenvolverá irá receber como entrada um arquivo de texto (.txt)
+#contendo vários conjuntos de dados e várias operações. Estas operações e dados estarão representadas
+#em um arquivo de textos contendo apenas os dados referentes as operações que devem ser realizadas
+#segundo a seguinte regra fixa: a primeira linha do arquivo de texto de entrada conterá o número de
+#operações que estão descritas no arquivo, este número de operações será um inteiro; as linhas
+#seguintes seguirão sempre o mesmo padrão de três linhas: a primeira linha apresenta o código da
+#operação (U para união, I para interseção, D para diferença e C produto cartesiano), a segunda e
+#terceira linhas conterão os elementos dos conjuntos separados por virgulas. A seguir está um exemplo
+#das linhas que podem existir em um arquivo de testes para o programa que você irá desenvolver:
+
+#Leonardo Lino Guedes
+
 #Método correto para se abrir um conjunto em python
 conjunto1 = set()
 conjunto2 = set()
